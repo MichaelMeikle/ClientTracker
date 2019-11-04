58a56d139b77ce2d4c085f9d19aaadf2f0492714
MichaelMeikle <mmeikle3@gmail.com>
2019-11-04 02:39:24 -0500

    Actual initial commit with proper structure

---
+++ src/main.h
@@ -0,0 +1,33 @@
+#pragma once
+/***
+Developed by Michael T. Meikle
+
+Description:
+This plugin aims to aid TeamSpeak server admins in tracking what transpires on their server further than the typical logs allow. This plugin is designed
+to operate on a bot client, which will log relevant events associated with a TeamSpeak UID and store the events in a database that can be queried at a 
+later date by server admins. Initially will function of a local database with plugin commands, later intended to be used via a web interface and cloud db.
+***/
+
+#include <string>
+#include "teamspeak.h"
+class Main
+{
+public:
+	const std::string kPluginName = "Client Tracker";
+	const std::string kPluginAuthor = "Michael Meikle";
+	const std::string kPluginDescription = "Plugin to log client events to a database for later review via command or API";
+	const std::string kPluginVersion = "0.0.1";
+
+	int Initialize();
+	void Shutdown();
+	void DisplayNameChangeEvent(uint64 server_handle, anyID client_id, const char* display_name, const char* client_uid);
+	void ChangeChannelEvent(uint64 server_handle, anyID client_id, uint64 old_ch_id, uint64 new_ch_id, int visibility, const char* move_message);
+	void ChangeChannelEvent(uint64 server_handle, anyID client_id, uint64 old_ch_id, uint64 new_ch_id, int visibility, anyID mover_id, const char* mover_name, const char* mover_uid, const char* move_message);
+
+	/*** Getters ***/
+
+	/*** Setters ***/
+
+private:
+
+};
---
+++ src/teamspeak.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "teamspeak/public_errors.h"
+#include "teamspeak/public_errors_rare.h"
+#include "teamspeak/public_definitions.h"
+#include "teamspeak/public_rare_definitions.h"
+#include "teamspeak/clientlib_publicdefinitions.h"
+#include "ts3_functions.h"
+#include "plugin.h"
